184816521d6259ca8ed7aeddb32ce1795c9be071
pacihub <pacihub@users.noreply.github.com>
2020-06-01 06:48:55 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include<math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -12,7 +13,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         {
 
             average = (image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.00;
-int ave = round(average);
+          int ave = round(average);
 
             image[i][j].rgbtRed = ave;
             image[i][j].rgbtBlue = ave;
@@ -27,7 +28,7 @@ int ave = round(average);
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+
     for(int i=0; i<height; i++)
     {
 
@@ -38,11 +39,9 @@ float Rf,Gf,Bf;
             Rf = 0.393*image[i][j].rgbtRed + 0.189*image[i][j].rgbtBlue + 0.769*image[i][j].rgbtGreen;
             Gf = 0.349*image[i][j].rgbtRed + 0.168*image[i][j].rgbtBlue + 0.686*image[i][j].rgbtGreen;
             Bf = 0.272*image[i][j].rgbtRed + 0.131*image[i][j].rgbtBlue + 0.534*image[i][j].rgbtGreen;
-            
-            int R = round(Rf);
-            int G = round(Gf);
-            int B = round(Bf);
-            
+
+            int R = round(Rf); int G = round(Gf); int B = round(Bf);
+
             if(R>255)
             {
                 R = 255;
@@ -55,22 +54,47 @@ float Rf,Gf,Bf;
             {
                 B = 255;
             }
-            
-            
+
             image[i][j].rgbtRed = R;
             image[i][j].rgbtBlue = B;
             image[i][j].rgbtGreen = G;
         }
 
     }
-    
-    
+
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE image2[height][width];
+
+    for(int i=0; i<height; i++)
+    {
+
+        for(int j=0; j<width; j++)
+        {
+
+            image2[i][j].rgbtRed = image[i][width-1-j].rgbtRed ;
+            image2[i][j].rgbtBlue = image[i][width-1-j].rgbtBlue ;
+            image2[i][j].rgbtGreen = image[i][width-1-j].rgbtGreen ;
+        }
+
+    }
+
+    for(int i = 0; i<height; i++)
+    {
+        for(int j=0; j<width; j++)
+        {
+            image[i][j].rgbtRed = image2[i][j].rgbtRed;
+            image[i][j].rgbtBlue = image2[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = image2[i][j].rgbtGreen;
+        }
+    }
+
+
     return;
 }
 
