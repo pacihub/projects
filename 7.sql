850f6992647ad82031437c533dc921159c369424
pacihub <pacihub@users.noreply.github.com>
2020-07-06 05:52:38 +0000

    automated commit by check50 [check50=True]

--- 7.sql
+++ 7.sql
@@ -2,7 +2,7 @@ SELECT title, ratings.rating FROM movies
 JOIN ratings ON ratings.movie_id = movies.id 
 
 WHERE movies.year = "2010" and ratings.rating IS NOT NULL
-ORDER BY ratings.rating DESC, title;
+ORDER BY rating DESC, title;
 
 /* groups first by rating and then title is alphabetical
 
