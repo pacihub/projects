38b087102c000f2a6f087791840de0ba09ec0ff8
pacihub <pacihub@users.noreply.github.com>
2020-09-11 13:55:57 +0000

    automated commit by check50 [check50=True]

--- 12.sql
+++ 12.sql
@@ -1,10 +1,17 @@
-SELECT title from movies, people, stars
+SELECT title from movies
 
-WHERE movies.id = stars.movie_id and people.id = stars.person_id and people.name = "Johnny Depp" 
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON people.id = stars.person_id
 
+
+WHERE people.name = "Johnny Depp"
 
 INTERSECT
 
-SELECT title from movies, people, stars
-WHERE movies.id = stars.movie_id and people.id = stars.person_id and people.name = "Helena Bonham Carter";
+SELECT title from movies
 
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON people.id = stars.person_id
+
+
+WHERE people.name = "Helena Bonham Carter"
