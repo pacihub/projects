6716f0a07ffa1966a8b581646543752342e43b39
pacihub <pacihub@users.noreply.github.com>
2020-09-14 10:05:18 +0000

    automated commit by check50 [check50=True]

--- 13.sql
+++ 13.sql
@@ -1,5 +1,6 @@
-SELECT * from movies
-Where movies.id in (select movies.id from movies 
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON people.id = stars.person_id
-WHERE name = "Kevin Bacon" and birth = "1956");
+SELECT DISTINCT name from people
+JOIN stars on stars.person_id = people.id 
+WHERE name != "Kevin Bacon"
+AND movie_id IN (SELECT movie_id FROM people
+JOIN stars ON stars.person_id = people.id
+WHERE name = "Kevin Bacon" and birth = 1958)
