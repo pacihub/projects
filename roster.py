38a8a1a3983de0ed9bb9d65658ab6654880086d3
pacihub <pacihub@users.noreply.github.com>
2020-09-29 08:30:34 +0000

    automated commit by check50 [check50=True]

--- roster.py
+++ roster.py
@@ -8,11 +8,11 @@ def main():
     if len(argv) != 2:
        print("Usage: error")
        exit(1)
-       
+
     db = SQL("sqlite:///students.db")
-    
+
     query_house = argv[1]
-    
+
     rows = db.execute("SELECT * from students WHERE house = ? ORDER BY last, first, birth", query_house)
     for row in rows:
         first, middle, last, birth = row['first'], row["middle"], row["last"], row["birth"]
@@ -20,3 +20,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#i have to query the database that I just imported into, to give me names and birth year of students living in a particular
+#house. The query house is in argv, i type it.
+#so for every row iterate and define the values again for first, middle, last, birth
+#then I print with condition. {middle + ' ' if middle else ''} this read as:
+#print middle and space if there is middle, ELSE just print space.
